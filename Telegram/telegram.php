7057b0bf141e2190fac42c2c272acd2734508246
FreeDOSSS <dima15521@gmail.com>
2019-06-28 16:24:00 +0300

    teltega

---
+++ Telegram/telegram.php
@@ -0,0 +1,28 @@
+<?php
+
+/* https://api.telegram.org/botXXXXXXXXXXXXXXXXXXXXX/getUpdates,
+где, XXXXXXXXXXXXXXXXXXXXXXX - токен вашего бота, полученный ранее */
+
+$name = $_POST['name'];
+$phone = $_POST['phone'];
+$email = $_POST['email'];
+$token = "878030265:AAECuHmYBV1ynrUeMR0R_62ZLBFDNivWNDM";
+$chat_id = "-372222335";
+$arr = array(
+  'Имя пользователя: ' => $name,
+  'Телефон: ' => $phone,
+  'Email' => $email
+);
+
+foreach($arr as $key => $value) {
+  $txt .= "<b>".$key."</b> ".$value."%0A";
+};
+
+$sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
+
+if ($sendToTelegram) {
+  header('Location: Thanks.html');
+} else {
+  echo "Error";
+}
+?>
