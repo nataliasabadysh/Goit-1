0dcd7ca512b9b4b49b9e2c79da784dacd02d4fc0
FreeDOSSS <dima15521@gmail.com>
2019-10-14 12:19:23 +0300

    Updates

---
+++ precache-manifest.c281deaf8496c033e3d7c8b3a8f944ee.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc3c92372ddb5042b0844262637cf37e",
+    "url": "/Goit/my-app/index.html"
+  },
+  {
+    "revision": "c59fb2214bd6884f1975",
+    "url": "/Goit/my-app/static/css/main.c87db0fc.chunk.css"
+  },
+  {
+    "revision": "127cd32abfcfb367d94a",
+    "url": "/Goit/my-app/static/js/2.f2268752.chunk.js"
+  },
+  {
+    "revision": "c59fb2214bd6884f1975",
+    "url": "/Goit/my-app/static/js/main.b9f4ac31.chunk.js"
+  },
+  {
+    "revision": "0efee18b6b1d42d33843",
+    "url": "/Goit/my-app/static/js/runtime-main.45ae58a7.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Goit/React/hw1/precache-manifest.c784abfe8d3a6cc1ac4247d3563a33ca.js"
+  "/Goit/my-app/precache-manifest.c281deaf8496c033e3d7c8b3a8f944ee.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Goit/React/hw1/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Goit/my-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
